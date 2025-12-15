df302930c442f4f3dd210c9e429f052cead9ef88
Jferpon <jferpon089@g.educaand.es>
2025-12-15 13:26:35 +0100

    Commit 10

--- tests/test_suma.sh
+++ tests/test_suma.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Suma de ejemplo
-RESULTADO=$((2 + 3))
+# Valores a probar
+A=2
+B=3
+RESULTADO_ESPERADO=6
 
-echo "Probando suma: 2 + 3 = $RESULTADO"
+# Suma
+RESULTADO=$((A + B))
 
-# Condición intencionalmente incorrecta
-if [ "$RESULTADO" -eq 6 ]; then
+echo "Probando suma: $A + $B = $RESULTADO"
+
+# Test
+if [ "$RESULTADO" -eq "$RESULTADO_ESPERADO" ]; then
     echo "TEST OK"
     exit 0
 else
     echo "TEST FAIL"
     exit 1
 fi
-
