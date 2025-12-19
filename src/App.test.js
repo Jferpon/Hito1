79fc93ef107b2555cca935c8bd2e180cc61bf8d3
Jferpon <jferpon089@g.educaand.es>
2025-12-19 14:26:19 +0100

    Arreglo tests tras minirreto

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Test roto a propósito para el mini-reto
-test('mini-reto: test roto a propósito', () => {
+test('muestra el texto OK', () => {
   render(<App />);
-  // Esto va a fallar porque el texto 'ERROR' no existe
   const resultado = screen.getByTestId('resultado');
-  expect(resultado).toHaveTextContent('ERROR');
+  expect(resultado).toHaveTextContent('OK');
 });
